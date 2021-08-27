215f1fc9c96e3837012e4b413d831f14b15fada9
Tomas Capek <tomas@capek.io>
2021-08-27 15:13:45 +0200

    Add 'status' field to ValidationRequest.

--- website/api/models.py
+++ website/api/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class ValidationRequest(models.Model):
     class Status(models.TextChoices):
-        PENDING = 1
-        EXPIRED = 2
-        VALID = 3
-        FRAUD = 4
+        PENDING = "pending"
+        EXPIRED = "expired"
+        VALID = "valid"
+        FRAUD = "fraud"
 
     name = models.CharField(
         max_length=128
@@ -16,3 +16,4 @@ class ValidationRequest(models.Model):
     )
     data = models.TextField()
     date_sent = models.DateTimeField()
+    status = models.CharField(max_length=8, choices=Status.choices, default=Status.PENDING)
