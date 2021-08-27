94ab89a16753b77d82e80fa05ee59a740786c173
Tomas Capek <tomas@capek.io>
2021-08-27 15:35:44 +0200

    Create api/status endpoint.

--- website/api/urls.py
+++ website/api/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 
 from .views import get_request_status, create_new_request
 
 
 urlpatterns = [
-    url("create_request", create_new_request),
+    path("create_request", create_new_request),
+    path("status/<int:id>", get_request_status)
 ]
--- website/api/views.py
+++ website/api/views.py
@@ -1,8 +1,20 @@
 from django.http.response import JsonResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from .models import ValidationRequest
 from .serializers import ValidationRequestSerializer
+
+
+@api_view(["GET"])
+def get_request_status(request, id):
+    try:
+        validation_request = ValidationRequest.objects.get(pk=id)
+    except ValidationRequest.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    return Response({"status": validation_request.status}, status=status.HTTP_200_OK)
 
 
 @api_view(["POST"])
